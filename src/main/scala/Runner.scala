30dead48d73f848640a2e866c4abbcee67bbaf19
Simon Forster <sforster@academytrainee.com>
2021-02-08 10:37:57 +0000

    DUMMY CLASS

--- src/main/scala/Runner.scala
+++ src/main/scala/Runner.scala
@@ -1,5 +1,8 @@
 object Runner extends App{
 
   println("Running tests")
-
+  Radio.turnOn()
+  Radio.turnUp()
+  Radio.turnUp()
+  println(s"On is ${Radio.isOn()} and volume is ${Radio.getVolume()}")
 }
