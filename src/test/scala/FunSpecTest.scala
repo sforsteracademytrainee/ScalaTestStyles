6b1f861429a0f6b9d57f399a0c8b44cdcf0ba920
Simon Forster <sforster@academytrainee.com>
2021-02-08 11:51:15 +0000

    minor changes

--- src/test/scala/FunSpecTest.scala
+++ src/test/scala/FunSpecTest.scala
@@ -4,15 +4,17 @@ import org.scalatest.BeforeAndAfter
 class FunSpecTest extends AnyFunSpec {
   describe("A radio") {
     it("should turn on/off when toggled") {
+      Radio.reset()
       Radio.turnOff()
       assert(Radio.isOn() === false)
       Radio.turnOn()
       assert(Radio.isOn() === true)
     }
     it("should have it's volume increased by 1") {
-      var volume = Radio.getVolume()
+      Radio.reset()
+      var volume = Radio.getVolume
       Radio.turnUp()
-      assert(volume == (Radio.getVolume() + 1))
+      assert(Radio.getVolume == (volume + 1))
     }
   }
 }
--- src/test/scala/RadioFlatSpecTest.scala
+++ src/test/scala/RadioFlatSpecTest.scala
@@ -4,19 +4,22 @@ class RadioFlatSpecTest extends AnyFlatSpec
 {
   "A Radio" should "be off" in
   {
+    Radio.reset()
     Radio.turnOff()
     assert(Radio.isOn() === false)
   }
 
   "A Radio" should "be on" in
   {
+    Radio.reset()
     Radio.turnOn()
     assert(Radio.isOn() === true)
   }
 
   "Volume" should "go up to 1" in
   {
+    Radio.reset()
     Radio.turnUp()
-    assert(Radio.getVolume() === 1)
+    assert(Radio.getVolume === 1)
   }
 }
